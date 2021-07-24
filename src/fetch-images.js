24dc2383f993c01f94f5221d859de850343cd451
Ihor Kovalenko <kovalyan27@gmail.com>
2021-07-24 17:26:23 +0300

    Update function

---
+++ src/fetch-images.js
@@ -0,0 +1,43 @@
+const axios = require('axios');
+
+const key = '22609546-eb8daebfdd1e346a58a9b181d';
+
+const BASE_URL = 'https://pixabay.com/api/';
+
+
+export default class ImageApiService {
+  constructor() {
+    console.log(this);
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+    async getImages() {
+      try {
+        const url = `${BASE_URL}?key=${key}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=12`;
+        const response = await axios.get(url);
+        console.log(response);
+        this.page += 1;
+        console.log(this);
+        return response.data;
+      } catch (error) {
+        console.log(error);
+      }
+   }
+
+   resetPage() {
+    this.page = 1;
+   }
+
+   get query() {
+     return this.searchQuery;
+   }
+
+   set query(newQuery){
+    this.searchQuery = newQuery;
+   }
+ 
+
+}
+
+ 
